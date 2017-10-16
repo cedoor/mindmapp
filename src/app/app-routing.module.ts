c368cc2688254070da896d923fccd7e2387f48fe
cedoor <omardesogus9@gmail.com>
2017-10-16 22:24:46 +0200

    init angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import {HomeComponent} from "./components/home/home.component";
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, {useHash: true})],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
---
+++ src/typings.d.ts
@@ -0,0 +1,13 @@
+/* SystemJS module definition */
+declare var nodeModule: NodeModule;
+
+interface NodeModule {
+    id: string;
+}
+
+declare var window: Window;
+
+interface Window {
+    process: any;
+    require: any;
+}
