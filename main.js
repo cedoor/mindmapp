8fdfa36b951027447e1e8558f134fda89d1af8c0
cedoor <omardesogus9@gmail.com>
2017-11-16 15:55:39 +0100

    create initial loading screen

--- main.js
+++ main.js
@@ -1,40 +1,64 @@
-const electron = require("electron");
-const {app, BrowserWindow} = electron;
-
-let win, serve;
-
-const args = process.argv.slice(1);
-serve = args.some(val => val === "--dev");
+const electron = require("electron"),
+    {app, BrowserWindow} = electron,
+    path = require("path"),
+    args = process.argv.slice(1),
+    serve = args.some(val => val === "--dev");
 
 if (serve) require("electron-reload")(__dirname, {});
 
-function createWindow() {
+let mainWindow, loadingScreen;
 
-    const size = electron.screen.getPrimaryDisplay().workAreaSize;
+function createLoadingScreen() {
+    const entryPath = path.join("file://", __dirname, "loading.html");
 
-    // Create the browser window.
-    win = new BrowserWindow({
-        x: 0,
-        y: 0,
-        width: size.width, minWidth: 800,
-        height: size.height, minHeight: 700,
-        icon: __dirname + "/icon.png"
+    loadingScreen = new BrowserWindow({
+        icon: __dirname + "/icon.png",
+        width: 800,
+        height: 600,
+        frame: false
     });
 
-    // and load the index.html of the app.
-    win.loadURL("file://" + __dirname + "/index.html");
+    loadingScreen.loadURL(entryPath);
 
-    // Open the DevTools.
-    if (serve) {
-        win.webContents.openDevTools();
-    }
-
-    win.on("closed", () => {
-        win = null;
+    loadingScreen.on("closed", () => {
+        loadingScreen = null;
     });
 }
 
-app.on("ready", createWindow);
+function createMainWindow() {
+    const indexPath = path.join("file://", __dirname, "index.html"),
+        size = electron.screen.getPrimaryDisplay().workAreaSize;
+
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        icon: __dirname + "/icon.png",
+        x: 0, y: 0,
+        width: size.width, minWidth: 800,
+        height: size.height, minHeight: 700,
+        show: false
+    });
+
+    mainWindow.loadURL(indexPath);
+
+    // Open the DevTools.
+    if (serve) {
+        mainWindow.webContents.openDevTools();
+    }
+
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
+}
+
+app.on("ready", () => {
+    createLoadingScreen();
+    createMainWindow();
+
+    setTimeout(() => {
+        mainWindow.show();
+        loadingScreen.close();
+    }, 2000);
+});
 
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") {
