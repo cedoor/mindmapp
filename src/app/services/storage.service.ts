c898ed089427b283b52292f7f77f318d72cd5bae
cedoor <omardesogus9@gmail.com>
2018-01-08 15:49:13 +0100

    [fix] fix storage bug

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -3,101 +3,69 @@ import {Injectable} from "@angular/core";
 @Injectable()
 export class StorageService {
 
-    private storage;
+    private storage: Storage;
 
     constructor() {
-        this.storage = window.require("electron-json-storage");
-    }
-
-    getPath(): string {
-        return this.storage.getDefaultDataPath();
+        this.storage = localStorage;
     }
 
     get(keys: string | string[]): Promise<any | any[]> {
-        return new Promise<any>((resolve, reject) => {
+        return new Promise<any>((resolve) => {
             if (typeof keys === "string") {
-                this.storage.get(keys, (error, data) => {
-                    if (error) {
-                        reject(error);
-                    } else {
-                        resolve(data);
-                    }
-                });
+                resolve(JSON.parse(this.storage.getItem(keys)));
             } else {
-                this.storage.getMany(keys, (error, data) => {
-                    if (error) {
-                        reject(error);
-                    } else {
-                        resolve(data);
+                let items = [];
+                for (let i = 0, len = this.storage.length; i < len; ++i) {
+                    let key = this.storage.key(i);
+                    if (keys.indexOf(key) !== -1) {
+                        items.push(JSON.parse(this.storage.getItem(this.storage.getItem(key))));
                     }
-                });
+                }
+                resolve(items);
             }
         });
     }
 
     getAll(): Promise<any> {
-        return new Promise<any>((resolve, reject) => {
-            this.storage.getAll((error, data) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(data);
-                }
-            });
+        return new Promise<any>((resolve) => {
+            let items = [];
+            for (let i = 0, len = this.storage.length; i < len; ++i) {
+                let key = this.storage.key(i);
+                items.push(JSON.parse(this.storage.getItem(this.storage.getItem(key))));
+            }
+            resolve(items);
         });
     }
 
     set(key: string, data: any): Promise<any> {
-        return new Promise<any>((resolve, reject) => {
-            this.storage.set(key, data, (error) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(data);
-                }
-            });
+        return new Promise<any>((resolve) => {
+            data = JSON.stringify(data);
+            this.storage.setItem(key, data);
+            resolve(data);
         });
     }
 
     remove(key: string): Promise<any> {
-        return new Promise<any>((resolve, reject) => {
-            this.storage.remove(key, (error) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(key);
-                }
-            });
+        return new Promise<any>((resolve) => {
+            resolve(this.storage.removeItem(key));
         });
     }
 
     exist(key: string): Promise<any> {
-        return new Promise<any>((resolve, reject) => {
-            this.storage.has(key, (error, exist) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(exist);
-                }
-            });
+        return new Promise<any>((resolve) => {
+            resolve(!!this.storage.getItem(key));
         });
     }
 
     clear(): Promise<any> {
-        return new Promise<any>((resolve, reject) => {
-            this.storage.clear((error) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve();
-                }
-            });
+        return new Promise<any>((resolve) => {
+            resolve(this.storage.clear());
         });
     }
 
     empty(): Promise<any> {
         return this.getAll().then(data => {
-            return Object.keys(data).length === 0;
+            return data.length !== 0;
         });
     }
 
