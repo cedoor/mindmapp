c1246c3d87aaf209a525b22d2457c41e0fe312b1
Omar Desogus <omardesogus9@gmail.com>
2018-03-15 23:31:02 +0100

    [refactor] add hammer package for gestures

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import {enableProdMode} from "@angular/core";
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import "hammerjs";
 
 import {AppModule} from "./app/app.module";
-import { environment } from './environments/environment';
+import {environment} from "./environments/environment";
 
 if (environment.production) {
     enableProdMode();
