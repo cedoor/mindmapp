fe239dc310004660c8a378cc6437a6cea6a1b7de
Omar Desogus <omardesogus9@gmail.com>
2018-03-23 16:47:25 +0100

    [feat] create new about component

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from "@angular/core";
+import {UtilsService} from "../../services/utils.service";
+
+@Component({
+    selector: "app-about",
+    templateUrl: "./about.component.html",
+    styleUrls: ["./about.component.scss"]
+})
+export class AboutComponent implements OnInit {
+
+    public packageInformations: any;
+    public currentYear: string;
+
+    constructor(public utilsService: UtilsService) {
+    }
+
+    ngOnInit() {
+        this.utilsService.getPackageInformations().then((packageInformations: any) => {
+            this.packageInformations = packageInformations;
+        });
+
+        this.currentYear = new Date().getFullYear().toString();
+    }
+
+}
