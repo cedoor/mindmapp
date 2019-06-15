8b8ff88d14e49c4228a98fc72bf33e87c09df42e
cedoor <omardesogus9@gmail.com>
2019-06-15 13:04:34 +0200

    [refactor] update all util functions

--- src/app/core/services/utils.service.ts
+++ src/app/core/services/utils.service.ts
@@ -1,27 +1,47 @@
 import {Injectable} from '@angular/core'
-import * as packageInformations from '../../../../package.json'
+import {environment} from '../../../environments/environment'
 
 @Injectable({
     providedIn: 'root'
 })
 export class UtilsService {
 
-    private remote: Electron.Remote
-
-    private serviceCache: {
-        packageInformations
+    constructor () {
     }
 
-    constructor () {
-        if (window.require) {
-            this.remote = window.require('electron').remote
-        }
+    /**
+     * Upload a file with a fake input click.
+     */
+    public static uploadFile (accept: string[] | string = 'application/json'): Promise<string> {
+        return new Promise((resolve, reject) => {
+            const fakeInput = document.createElement('input')
 
-        this.serviceCache = {
-            packageInformations: false
-        }
+            fakeInput.type = 'file'
+            fakeInput.accept = Array.isArray(accept) ? accept.join(', ') : accept
 
-        this.serviceCache.packageInformations = packageInformations
+            document.body.appendChild(fakeInput)
+
+            fakeInput.click()
+
+            fakeInput.oninput = () => {
+                const fileReader = new FileReader()
+
+                fileReader.onload = () => {
+                    resolve(fileReader.result.toString())
+                }
+                fileReader.onerror = reject
+
+                if (accept === 'application/json') {
+                    fileReader.readAsText(fakeInput.files[0])
+                } else {
+                    fileReader.readAsDataURL(fakeInput.files[0])
+                }
+            }
+
+            fakeInput.onerror = reject
+
+            document.body.removeChild(fakeInput)
+        })
     }
 
     /**
@@ -56,9 +76,25 @@ export class UtilsService {
     }
 
     /**
+     * Return the status of the full screen mode.
+     */
+    public static isFullScreen (): boolean {
+        return window.document.fullscreenEnabled
+    }
+
+    /**
+     * Toggle the full screen mode.
+     */
+    public static toggleFullScreen () {
+        if (UtilsService.isFullScreen()) {
+            window.document.exitFullscreen()
+        } else {
+            window.document.querySelector('html').requestFullscreen()
+        }
+    }
+
+    /**
      * Return true if the string is a JSON Object.
-     * @param {string} JSONString
-     * @returns {boolean}
      */
     public static isJSONString (JSONString: string) {
         try {
@@ -70,69 +106,28 @@ export class UtilsService {
     }
 
     /**
-     * Open a link with the default browser.
-     * @param {string} href
+     * Open a link with the default browser (for Electron application).
      */
-    public openExternalLink (href: string) {
-        this.remote.shell.openExternal(href)
-    }
-
-    /**
-     * Close the application.
-     */
-    public closeApplication () {
-        const currentWindow = this.remote.getCurrentWindow()
-
-        currentWindow.close()
-    }
-
-    /**
-     * Minimize the application.
-     */
-    public minimizeApplication () {
-        const currentWindow = this.remote.getCurrentWindow()
-
-        currentWindow.minimize()
-    }
-
-    /**
-     * Invert the full screen setting of the application.
-     */
-    public toggleFullScreen () {
-        const currentWindow = this.remote.getCurrentWindow()
-
-        if (this.isFullScreen()) {
-            currentWindow.setFullScreen(false)
+    public static openExternalLink (href: string) {
+        if (environment.electron) {
+            window.require('electron').remote.shell.openExternal(href)
         } else {
-            currentWindow.setFullScreen(true)
+            window.open(href)
         }
     }
 
     /**
-     * Return the status of the fullScreen setting.
-     * @returns {boolean}
-     */
-    public isFullScreen (): boolean {
-        const currentWindow = this.remote.getCurrentWindow()
-
-        return currentWindow.isFullScreen()
-    }
-
-    /**
      * Return true if the two objects have the same structure (same keys).
-     * @param {object} JSONObject1
-     * @param {object} JSONObject2
-     * @returns {boolean}
      */
-    public haveSameStructure (JSONObject1: object, JSONObject2: object): boolean {
-        const fun = (JSONObject1: object, JSONObject2: object) => {
-            for (const key in JSONObject1) {
-                if (!JSONObject1.hasOwnProperty(key) || !JSONObject2.hasOwnProperty(key)) {
+    public static isSameJSONStructure (json1: object, json2: object): boolean {
+        function checkObjectStructure (object1: object, object2: object): boolean {
+            for (const key in object1) {
+                if (!object1.hasOwnProperty(key) || !object2.hasOwnProperty(key)) {
                     return false
                 }
 
-                if (typeof JSONObject1[key] === 'object') {
-                    if (!fun(JSONObject1[key], JSONObject2[key])) {
+                if (typeof object1[key] === 'object') {
+                    if (!checkObjectStructure(object1[key], object2[key])) {
                         return false
                     }
                 }
@@ -141,7 +136,7 @@ export class UtilsService {
             return true
         }
 
-        return fun(JSONObject1, JSONObject2) && fun(JSONObject2, JSONObject1)
+        return checkObjectStructure(json1, json2) && checkObjectStructure(json2, json1)
     }
 
 }
