8f7570b55125c9ae6b07e603b4d2f4370357355e
cedoor <omardesogus9@gmail.com>
2019-06-22 14:54:53 +0200

    [style] update structure of pages

--- src/app/shared/animations/route.animation.ts
+++ src/app/shared/animations/route.animation.ts
@@ -6,7 +6,6 @@ export const routeAnimation = trigger('routeAnimation', [
         query(':enter, :leave', [
             style({
                 position: 'absolute',
-                left: 0,
                 width: '100%',
                 height: '100%',
                 opacity: 0
