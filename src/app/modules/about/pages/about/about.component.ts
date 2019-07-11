7c825ba2f72f95024ef735c22096bff67ba856f3
cedoor <omardesogus9@gmail.com>
2019-07-11 12:21:53 +0200

    [refactor] remove menu from about header

--- src/app/modules/about/pages/about/about.component.ts
+++ src/app/modules/about/pages/about/about.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core'
 import {UtilsService} from '../../../../core/services/utils/utils.service'
 import * as packageJson from '../../../../../../package.json'
-import {SidenavService} from '../../../../core/services/sidenav/sidenav.service'
 
 @Component({
     selector: 'mindmapp-about',
@@ -15,7 +14,7 @@ export class AboutComponent implements OnInit {
     public projectAuthor: string
     public projectRepositoryUrl: string
 
-    constructor (private sidenavService: SidenavService) {
+    constructor () {
         this.projectName = UtilsService.capitalizeWord(packageJson.name)
         this.projectRepositoryUrl = packageJson.repository.url
         this.projectAuthor = packageJson.author.name
@@ -23,10 +22,6 @@ export class AboutComponent implements OnInit {
 
     public ngOnInit () {
         this.currentYear = new Date().getFullYear().toString()
-    }
-
-    public openSidenav () {
-        this.sidenavService.open()
     }
 
     public slide (selector: string, event: Event) {
