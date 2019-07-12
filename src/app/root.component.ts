47f1ec91616e46d078839025f623abd8c0e079d8
cedoor <omardesogus9@gmail.com>
2019-07-12 12:57:17 +0200

    [feat] set browser language the first time

--- src/app/root.component.ts
+++ src/app/root.component.ts
@@ -1,9 +1,8 @@
-import {Component, OnInit, ViewChild} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 import {ShortcutsService} from './core/services/shortcuts/shortcuts.service'
 import {TranslateService} from '@ngx-translate/core'
 import {SettingsService} from './core/services/settings/settings.service'
 import {NotificationService} from './core/services/notification/notification.service'
-import {RouterOutlet} from '@angular/router'
 import {routeAnimation} from './shared/animations/route.animation'
 
 @Component({
@@ -14,9 +13,6 @@ import {routeAnimation} from './shared/animations/route.animation'
 })
 export class RootComponent implements OnInit {
 
-    @ViewChild('outlet', {static: false}) public outlet: RouterOutlet
-    public initialized: boolean
-
     constructor (private notificationService: NotificationService,
                  private translateService: TranslateService,
                  private settingsService: SettingsService,
@@ -25,6 +21,12 @@ export class RootComponent implements OnInit {
 
     public async ngOnInit () {
         const {settings, isFirstTime} = await this.settingsService.init()
+
+        if (isFirstTime) {
+            settings.general.language = this.translateService.getBrowserLang()
+            await this.settingsService.updateCachedSettings(settings)
+        }
+
         await this.initTranslations(settings.general.language)
 
         this.shortcutsService.init()
@@ -33,8 +35,6 @@ export class RootComponent implements OnInit {
         if (isFirstTime) {
             this.notificationService.showSnackBarMessage('WELCOME_MESSAGE')
         }
-
-        this.initialized = true
     }
 
     private async initTranslations (language: string): Promise<void> {
