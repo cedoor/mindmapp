96c45bd62a7f3e1345f64dc408929562e39a8b2b
cedoor <omardesogus9@gmail.com>
2019-08-19 16:54:13 +0200

    [refactor] update router urls

--- src/app/root-routing.module.ts
+++ src/app/root-routing.module.ts
@@ -3,17 +3,13 @@ import {RouterModule, Routes} from '@angular/router'
 
 const routes: Routes = [{
     path: '',
-    redirectTo: 'application',
-    pathMatch: 'full'
-}, {
-    path: 'application',
-    loadChildren: () => import('./modules/application/application.module').then(m => m.ApplicationModule)
-}, {
-    path: 'about',
     loadChildren: () => import('./modules/about/about.module').then(m => m.AboutModule)
 }, {
+    path: 'app',
+    loadChildren: () => import('./modules/application/application.module').then(m => m.ApplicationModule)
+}, {
     path: '**',
-    redirectTo: 'application'
+    redirectTo: ''
 }]
 
 @NgModule({
