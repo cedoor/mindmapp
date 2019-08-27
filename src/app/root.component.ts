35557a309d491b53e139cd33e0708207112b71aa
cedoor <omardesogus9@gmail.com>
2019-08-27 11:58:16 +0200

    [feat] go to application page as default in PWA

--- src/app/root.component.ts
+++ src/app/root.component.ts
@@ -4,6 +4,7 @@ import {TranslateService} from '@ngx-translate/core'
 import {SettingsService} from './core/services/settings/settings.service'
 import {NotificationService} from './core/services/notification/notification.service'
 import {routeAnimation} from './shared/animations/route.animation'
+import {Router} from '@angular/router'
 
 @Component({
     selector: 'mindmapp-root',
@@ -15,6 +16,7 @@ export class RootComponent implements OnInit {
 
     constructor (private notificationService: NotificationService,
                  private translateService: TranslateService,
+                 private router: Router,
                  private settingsService: SettingsService,
                  private shortcutsService: ShortcutsService) {
     }
@@ -30,6 +32,13 @@ export class RootComponent implements OnInit {
         await this.initTranslations(settings.general.language)
 
         this.shortcutsService.init()
+
+        // If there is a PWA environment go to application page as default.
+        if (window.matchMedia('(display-mode: standalone)').matches) {
+            this.router.navigate(['app'], {
+                replaceUrl: true
+            })
+        }
 
         if (isFirstTime) {
             this.notificationService.showSnackBarMessage('MESSAGES.WELCOME_MESSAGE')
